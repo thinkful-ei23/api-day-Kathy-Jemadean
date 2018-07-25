897f500100b6e7dfe147d3fa4088648299fb5170
Jemadean Dobrosielski <jemadean@gmail.com>
2018-07-25 09:31:53 -0700

    wrote getItems method

--- scripts/api.js
+++ scripts/api.js
@@ -4,8 +4,8 @@
 const api = (function() {
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/kathy-jemadean';
   const getItems = function(callback) {
-    callback('api module works!');
-
+    $.getJSON(`${BASE_URL}/items`, callback);
+    //callback('api module works!');
   };
 
   return{
